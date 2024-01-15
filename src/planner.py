0af8b1712d7c0eb802f131c32d72fc1ab03b1d37
sebacastillo <castilloclaudiosebastian@gmail.com>
2024-01-15 15:03:33 -0300

    update

--- src/planner.py
+++ src/planner.py
@@ -66,3 +66,45 @@ kernel.add_chat_service(
 skill = kernel.import_semantic_skill_from_directory("samples/skills", "FunSkill")
 joke_function = skill["Joke"]
 print(joke_function("soccer in Argentina"))
+
+
+# Planner------------------------------------------------------------------------------------
+# 
+# https://github.com/microsoft/semantic-kernel/blob/main/python/samples/kernel-syntax-examples/azure_chat_gpt_with_data_api.py
+
+
+
+async def main():
+    import semantic_kernel as sk
+    from plugins.MathPlugin.Math import Math
+    from semantic_kernel.planning.sequential_planner import SequentialPlanner
+    import config.add_completion_service
+
+    # Initialize the kernel
+    kernel = sk.Kernel()
+    # Add a text or chat completion service using either:
+    # kernel.add_text_completion_service()
+    # kernel.add_chat_service()
+    kernel.add_completion_service()
+
+    # Import the native functions
+    math_plugin = kernel.import_skill(Math(), "MathPlugin")
+
+    planner = SequentialPlanner(kernel)
+
+    ask = "If my investment of 2130.23 dollars increased by 23%, how much would I have after I spent $5 on a latte?"
+
+    # Create a plan
+    plan = await planner.create_plan_async(ask)
+
+    # Execute the plan
+    result = await plan.invoke_async()
+    print("Plan results:")
+    print(result)
+
+
+# Run the main function
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
