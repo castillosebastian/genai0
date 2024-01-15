2491f04a73e91cd424c6ed0775866fb646ee7760
sebacastillo <castilloclaudiosebastian@gmail.com>
2024-01-15 18:07:08 -0300

    upload

---
+++ src/math.py
@@ -0,0 +1,2 @@
+# https://learn.microsoft.com/en-us/semantic-kernel/agents/plugins/?tabs=python
+#https://learn.microsoft.com/en-us/semantic-kernel/agents/plugins/using-the-kernelfunction-decorator?tabs=python
